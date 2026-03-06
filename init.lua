16ac7931d1e6950bc19602cb1a0cf42bc6fd3779
BeentageBand <beentageband@gmail.com>
2026-03-06 09:17:57 -0800

    add kubernetes

--- init.lua
+++ init.lua
@@ -730,6 +730,19 @@ require('lazy').setup({
           settings = {
             yaml = {
               schemas = {
+                kubernetes = '*.yaml',
+                ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*.{yml,yaml}',
+                ['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
+                ['http://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/*.{yml,yaml}',
+                ['http://json.schemastore.org/prettierrc'] = '.prettierrc.{yml,yaml}',
+                ['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
+                ['http://json.schemastore.org/ansible-playbook'] = '*play*.{yml,yaml}',
+                ['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
+                ['https://json.schemastore.org/dependabot-v2'] = '.github/dependabot.{yml,yaml}',
+                ['https://json.schemastore.org/gitlab-ci'] = '*gitlab-ci*.{yml,yaml}',
+                ['https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json'] = '*api*.{yml,yaml}',
+                ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = '*docker-compose*.{yml,yaml}',
+                ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = '*flow*.{yml,yaml}',
                 ['~/.local/share/dab-schema.json'] = '*.yml',
               },
             },
@@ -795,22 +808,22 @@ require('lazy').setup({
             },
           },
         },
-        groovyls = {
-          cmd = {
-            '/usr/bin/java',
-            '-Dlog.level=FINE',
-            '-jar',
-            vim.fn.stdpath 'data' .. '/mason/packages/groovyls/groovyls-all.jar',
-          },
-          capabilities = capabilities,
-          settings = {
-            groovy = {
-              classpath = {
-                '/usr/share/groovy/lib/groovy-json.jar',
-              },
-            },
-          },
-        },
+        --groovyls = {
+        --  cmd = {
+        --    '/usr/bin/java',
+        --    '-Dlog.level=FINE',
+        --    '-jar',
+        --    vim.fn.stdpath 'data' .. '/mason/packages/groovyls/groovyls-all.jar',
+        --  },
+        --  capabilities = capabilities,
+        --  settings = {
+        --    groovy = {
+        --      classpath = {
+        --        '/usr/share/groovy/lib/groovy-json.jar',
+        --      },
+        --    },
+        --  },
+        --},
       }
 
       -- Ensure the servers and tools above are installed
